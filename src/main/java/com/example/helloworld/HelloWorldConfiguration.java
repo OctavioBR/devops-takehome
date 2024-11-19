a7d6b13ba658c28261da4f7205a30c568ebfbfde
Octávio Richter <4429577+OctavioBR@users.noreply.github.com>
2024-11-19 03:07:21 -0300

    revert me: code change

--- src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import io.dropwizard.core.Configuration;
 
 public class HelloWorldConfiguration extends Configuration {
-    private String content = "Stranger";
+    private String content = "Strangers";
 
     public String getContent() {
         return content;
